1d2fe1ceedad5ab15c486081bf59f98287a0107d
kuikelaashish <92312527+kuikelaashish@users.noreply.github.com>
2022-10-07 15:20:25 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Script running....")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if()document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display='none'
+        document.querySelector('.cross').style.display='inline'
+    }
+    else{
+        document.querySelector('.cross').style.display='none'
+        document.querySelector('.ham').style.display='inline'
+    }, 350);
+}) 
